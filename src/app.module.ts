210416a1c8bacf5a0989209f27b44c66f1692266
Aurora Tulilaulu <aurora.tulilaulu@codemate.com>
2022-01-27 14:12:34 +0200

    started work

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GamesController } from './games/games.controller';
+import { GamesService } from './games/games.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TypeOrmModule.forRoot(), UsersModule],
+  controllers: [AppController, GamesController],
+  providers: [AppService, GamesService],
 })
 export class AppModule {}
